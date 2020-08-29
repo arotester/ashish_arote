2fcae0712007509c60a820ac4e05f7435afe56d5
Ashish Arote <68053186+arotester@users.noreply.github.com>
2020-08-29 12:23:01 +0530

    Update name.php

--- name.php
+++ name.php
@@ -1,33 +1,15 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent = "From: $name \n email: $email \n Message: $message";
-$recipient = 'ashisharote4@gmail.com';
-$subject = 'Message from Website';
-$mailheader = "From: $email \r\n";
-$captcha;
-{
-  if(isset($_POST['g-recaptcha-response']))
-    {
-      $captcha=$_POST['g-recaptcha-response'];
-    }
-  if(!$captcha)
-    {
-      echo '<script language="javascript">';
-      echo 'alert("Please check the Captcha")';
-      echo '</script>';
-      exit;  
-    }
-  $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
-  if($response.success==false)
-  {
-    echo '<h2>Please do not try and spam here.</h2>';
-  }else
-  {
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo '<script language="javascript">';
-echo 'alert("Your Message successfully sent, we will get back to you ASAP.");';
-echo 'window.location.href="index.html";';
-echo '</script>';
-  }
-} ?>
+<?php
+firma = $_POST['firma'];
+name = $_POST['name'];
+email = $_POST['email'];
+phone = $_POST['phone'];
+wnio = $_POST['wnio'];
+kwota = $_POST['kwota'];
+message = $_POST['message'];
+formcontent=" Nazwa Firmy: $firma \n Imie i Nazwisko: $name \n Email: $email \n Telefon: $phone \n Wniosek: $wnio \n Prognozowana Kwota: $kwota \n Wiadomość: $message";
+recipient = "<ashisharote4@gmail.com>";
+subject = "Formularz Kontaktowy";
+mailheader = "From: $email";
+mail($recipient, $subject, $formcontent, $mailheader);
+exit();//Remove this after debugging done
+?>
